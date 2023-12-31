94f345a7e48724ac70826880aa060c642e2f4b64
jin567 <jinzhiyuan567@gmail.com>
2023-12-31 14:01:32 +0800

    测试从训练集中随机选择5个元素当成训练 完成低频采样的恢复(5个点恢复成10个点)

---
+++ test_api.py
@@ -0,0 +1,23 @@
+import torch
+import numpy as np
+
+
+def gen_new_tensor(tensor):
+    # 随机选择五个索引
+    indices = torch.randperm(tensor.size(1))[:5]
+    print(indices)
+    # 从原始张量中提取选定的五个元素
+    selected_elements = tensor[:, indices]
+
+    # 将选定的五个元素变成 64x5 的张量
+    new_tensor = selected_elements.view(2, 5)
+    return new_tensor
+
+
+if __name__ == '__main__':
+    tensor = torch.rand(2, 10)
+    for i in range(5):
+        new_tensor = gen_new_tensor(tensor)
+        print(tensor, tensor.shape)
+        print(new_tensor, new_tensor.shape)
+        print("---------------------------------")
